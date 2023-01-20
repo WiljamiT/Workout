d78ac33d9d30b44f6c24435fa49cb0957e6d2063
WiljamiT <74732018+WiljamiT@users.noreply.github.com>
2023-01-20 12:11:43 +0200

    search ui

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material'; 
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <Stack 
+        alignItems='center' 
+        mt='40px' 
+        justifyContent='center' 
+        p='20px'
+    >
+        <Typography 
+            fontWeight='700px' sx={{ fontSize: { lg: '44px', xs: '30px' }}} 
+            mb='50px' 
+            textAlign='center'
+        >
+            Tekstiä <br /> Tekstiä
+        </Typography>
+        <Box position='relative' mb='72px'>
+            <TextField 
+                sx={{
+                    input: { fontWeight: '700px', borderRadius: '15px' },
+                    width: { lg: '700px', xs: '350px'},
+                    backgroundColor: '#FFFFFF'
+                }}
+                height='80px'
+                value=''
+                onChange={(e) => {}}
+                placeholder='Search Exercises'
+                type='text'
+            />
+            <Button 
+                variant='contained' 
+                sx={{ 
+                    textTransform: 'none', 
+                    width: { lg: '175px', xs: '80px'}, 
+                    fontSize: { lg: '20px', xs: '14px'}, 
+                    height: '56px', position: 'absolute', right: '0'
+                    }}
+                >
+                Search
+            </Button>
+        </Box>
+
+    </Stack>
   )
 }
 
